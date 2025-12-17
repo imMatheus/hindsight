41fd2121f30ec00b73c405b1fc0023b956770ba0
imMatheus <matheusmendesbarata03@gmail.com>
2025-12-17 03:44:03 -0800

    removed dumb logs

--- server/main.go
+++ server/main.go
@@ -198,14 +198,12 @@ func cloneRepo(repoURL string) ([]CommitStats, map[string]int, error) {
 	// var gitLogStderr bytes.Buffer
 	// cmd.Stderr = &gitLogStderr
 	output, err := cmd.Output()
-	log.Printf("MATHEUS TEST [%s]", repoURL)
 
 	if err != nil {
 		log.Printf("Git log failed for %s: %v ", repoURL, err)
 		// log.Printf("Git log failed for %s: %v - stderr: %s", repoURL, err, gitLogStderr.String())
 		return nil, nil, err
 	}
-	log.Printf("ADAM TEST [%s]", repoURL)
 	elapsed = time.Since(startTime)
 	log.Printf("Successfully ran git log for repository [%s] in %s", repoURL, elapsed)
 
